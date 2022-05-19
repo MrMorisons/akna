619c428aea9cf9a06c635e5153f89c9e6917bfff
MrMorisons <91465005+MrMorisons@users.noreply.github.com>
2022-05-19 16:35:30 +0200

    widget No.1
    ideiglenesen kész a widget.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,32 @@
 using namespace genv;
 
 class widget{
+protected:
     int x;
     int y;
     int sx;
     int sy;
+    bool selected=false;
+public:
+    void setkord(int newx, int newy){
+        x=newx;
+        y=newy;
+    }
+    void setsize(int new_x, int new_y){
+        sx=new_x;
+        sy=new_y;
+    }
+    void kivalsztva(event ev){
+        if(ev.type==ev_mouse){
+            if(ev.pos_x >= x and ev.pos_y >= y and ev.pos_x  <= x+sx and ev.pos_y <= y+sy and ev.button<0){
+                selected=true;
+            }
+            else{
+                selected=false;
+            }
+        }
+    }
+    void rajz();
 };
 
 int main()
