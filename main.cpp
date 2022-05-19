2a2a1c58a283c95cd4ff55b2594d9d60533c3fe7
MrMorisons <91465005+MrMorisons@users.noreply.github.com>
2022-05-19 16:11:47 +0200

    start widget
    Elkezdtem a widget clast

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "graphics.hpp"
 using namespace genv;
 
+class widget{
+    int x;
+    int y;
+    int sx;
+    int sy;
+};
 
 int main()
 {
     gout.open(400,400);
-    gout <<text("hello world")<< refresh;
     event ev;
     while(gin >> ev) {
     }
